e68a765ae8887647ea15f2f8c6530f3b5b328b62
krisikostadinov <krisi.199898@gmail.com>
2025-01-16 21:48:27 +0200

    feat: add role management and authentication middleware; implement user role assignment on registration

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,6 +12,7 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
 
 type NavbarProps = {
   session: Session | null;
@@ -79,14 +80,24 @@ export default function Navbar({ session }: NavbarProps) {
                     Влизане
                   </Link>
                 ) : (
-                  <Link
-                    href="/account"
-                    className="flex w-full items-center py-2 text-lg font-semibold hover:bg-slate-100 px-5 rounded"
-                    prefetch={false}
-                    onClick={() => setIsOpen(false)}
-                  >
-                    Акаунт
-                  </Link>
+                  <>
+                    <Link
+                      href="/account"
+                      className="flex w-full items-center py-2 text-lg font-semibold hover:bg-slate-100 px-5 rounded"
+                      prefetch={false}
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Акаунт
+                    </Link>
+                    <Link
+                      href={"#"}
+                      className="flex w-full items-center py-2 text-lg font-semibold hover:bg-slate-100 px-5 rounded"
+                      prefetch={false}
+                      onClick={() => signOut()}
+                    >
+                      Изход
+                    </Link>
+                  </>
                 )}
               </div>
             </SheetDescription>
@@ -137,13 +148,23 @@ export default function Navbar({ session }: NavbarProps) {
             Влизане
           </Link>
         ) : (
-          <Link
-            href="/account"
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-            prefetch={false}
-          >
-            Акаунт
-          </Link>
+          <>
+            <Link
+              href="/account"
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              prefetch={false}
+            >
+              Акаунт
+            </Link>
+            <Link
+              href={"#"}
+              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+              prefetch={false}
+              onClick={() => signOut()}
+            >
+              Изход
+            </Link>
+          </>
         )}
       </nav>
     </header>
